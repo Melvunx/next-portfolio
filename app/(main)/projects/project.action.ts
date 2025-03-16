b9a8b8f211a7ca0546d9d0b8950cabd36029c562
Melvunx <augustine.melv@gmmail.com>
2025-03-16 16:18:14 +0100

    setup better auth config

---
+++ app/(main)/projects/project.action.ts
@@ -0,0 +1,64 @@
+import { prisma } from "@/src/lib/prisma";
+
+export const getProjectsAction = async () => {
+  try {
+    const projects = await prisma.project.findMany({
+      omit: {
+        projectStatusId: true,
+      },
+      include: {
+        status: {
+          select: {
+            name: true,
+          },
+        },
+        technologies: {
+          omit: {
+            technologyId: true,
+            projectId: true,
+          },
+          include: {
+            technology: {
+              omit: { categoryId: true },
+              include: {
+                category: true,
+              },
+            },
+          },
+        },
+        reactions: {
+          include: {
+            reaction: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return projects;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des projets:", error);
+    throw new Error("Impossible de récupérer les projets");
+  }
+};
+
+export const createProjectAction = async () => {};
+
+export const deleteProjectAction = async (projectId: string) => {
+  try {
+    await prisma.project.delete({
+      where: {
+        id: projectId,
+      },
+    });
+
+    return {
+      message: "Le projet a été supprimé avec succès !",
+    };
+  } catch (error) {
+    console.error("Erreur lors de la suppression du projet:", error);
+    throw new Error("Impossible de supprimer le projet");
+  }
+};
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,8 +2,30 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
 
+const {
+  BETTER_AUTH_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  CLIENT_GITHUB_ID,
+  CLIENT_GITHUB_SECRET,
+} = process.env;
+
 export const auth = betterAuth({
+  baseURL: BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: GOOGLE_CLIENT_ID as string,
+      clientSecret: GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: CLIENT_GITHUB_ID as string,
+      clientSecret: CLIENT_GITHUB_SECRET as string,
+    },
+  },
 });
