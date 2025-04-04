2c4355a2380baa66d0a103e367a1038d11bd8d6d
Melvunx <augustine.melv@gmmail.com>
2025-04-04 12:52:52 +0200

    add validation data

--- app/(main)/main.action.ts
+++ app/(main)/main.action.ts
@@ -1,6 +1,9 @@
 import { prisma } from "@/components/lib/prisma";
 import { LetterCreate, LetterCreateSchema } from "@/schema/letter";
+import { z } from "better-auth";
 import { redirect } from "next/navigation";
+
+export const idValidateSchema = z.string().uuid().nonempty();
 
 export async function sendLetterAction(credentials: LetterCreate) {
   try {
@@ -25,11 +28,35 @@ export async function sendLetterAction(credentials: LetterCreate) {
   redirect("/");
 }
 
+export async function getAccountIdAction(userId: string) {
+  try {
+    const validateUserId = idValidateSchema.parse(userId);
+
+    const accountId = await prisma.account.findFirst({
+      where: {
+        userId: validateUserId,
+      },
+      select: {
+        id: true,
+      },
+    });
+
+    if (!accountId) return null;
+
+    return accountId.id;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Erreur lors de la récupération de l'ID du compte");
+  }
+}
+
 export async function deleteUserAction(userId: string) {
   try {
+    const validateUserId = idValidateSchema.parse(userId);
+
     await prisma.user.delete({
       where: {
-        id: userId,
+        id: validateUserId,
       },
     });
   } catch (error) {
