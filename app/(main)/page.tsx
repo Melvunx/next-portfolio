d151fd01b684dea313eb24c82aabb60967321594
Melvunx <augustine.melv@gmmail.com>
2025-04-05 14:26:35 +0200

    create letter form

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,99 @@
-export default async function Page() {
-  return <div>Main page</div>;
+import { authUser } from "@/components/lib/auth-session";
+import { SubmitButton } from "@/components/SubmitButton";
+import { getAccountIdAction, sendLetterAction } from "./main.action";
+
+export default function Page() {
+  return (
+    <div>
+      Main page
+      {/* Faire un grid avec une belle présentation |me contacter | formulaire| */}
+      <form
+        action={async (data) => {
+          "use server";
+          const user = await authUser();
+
+          const accountId = user ? await getAccountIdAction(user.id) : null;
+
+          const credentials = {
+            sender: data.get("sender") as string,
+            email: data.get("email") as string,
+            object: data.get("object") as string,
+            message: data.get("message") as string,
+            accountId: accountId ? accountId : undefined,
+          };
+
+          await sendLetterAction(credentials);
+        }}
+      >
+        <div>
+          <div>
+            <label htmlFor="send" className="floating-label">
+              <span>Votre nom</span>
+              <input
+                type="text"
+                minLength={3}
+                maxLength={30}
+                pattern="^[a-zA-Z ]+$"
+                name="sender"
+                id="send"
+                className="input validator"
+                required
+              />
+              <p className="validator-hint">
+                Votre nom doit contenir entre 3 et 30 caractères et ne doit pas
+                contenir de caractères spéciaux.
+              </p>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="email" className="floating-label">
+              <span>Votre e-mail</span>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                maxLength={50}
+                className="input validator"
+                required
+              />
+              <p className="validator-hint">
+                Entrez une adresse e-mail valide.
+              </p>
+            </label>
+          </div>
+          <div>
+            <label htmlFor="obj" className="floating-label">
+              <span>Objet</span>
+              <input
+                type="text"
+                name="object"
+                id="obj"
+                minLength={3}
+                maxLength={50}
+                className="input validator"
+                required
+              />
+              <p className="validator-hint">
+                {"L'objet doit contenir entre 3 et 50 caractères."}
+              </p>
+            </label>
+          </div>
+        </div>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Votre message</legend>
+          <textarea
+            name="message"
+            className="textarea h-40 resize-none validator"
+            minLength={10}
+            maxLength={500}
+            required
+          ></textarea>
+          <p className="validator-hint">
+            Le message doit contenir entre 10 et 500 caractères.
+          </p>
+        </fieldset>
+        <SubmitButton />
+      </form>
+    </div>
+  );
 }
--- src/schema/letter.ts
+++ src/schema/letter.ts
@@ -1,12 +1,10 @@
 import { z } from "better-auth";
 
 export const LetterCreateSchema = z.object({
-  sender: z.string().min(3, "Le nom doit contenir au moins 3 caractères"),
-  email: z.string().email("Adresse e-mail invalide"),
-  object: z.string().min(3, "Le sujet doit contenir au moins 3 caractères"),
-  message: z
-    .string()
-    .min(10, "Le message doit contenir au moins 10 caractères"),
+  sender: z.string().min(3).max(30),
+  email: z.string().email().max(50),
+  object: z.string().min(3).max(50),
+  message: z.string().min(10).max(500),
   accountId: z.string().optional(),
 });
 
